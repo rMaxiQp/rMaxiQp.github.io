a05ee2f50cf4a72962846bc7f31bfda25f42287d
Zhengru Qian <zq2@illinois.edu>
2020-05-21 18:41:50 -0500

    Restart Managing CI pipeline

---
+++ .circleci/setup-github.sh
@@ -0,0 +1,23 @@
+
+git config user.name "${USER_NAME}"
+git config user.email "${USER_EMAIL}"
+
+git checkout master
+git log HEAD..origin/master
+
+# git pull -s recursive -X ours origin master
+
+git rebase --onto master develop
+
+# Remove all files except _site/* and dotfiles
+find . -maxdepth 1 ! -name '_site' ! -name '.*' -exec rm -rf {} \;
+mv _site/* .
+rm -R _site/
+
+# git reset --soft HEAD~1
+
+git add -A .
+
+git commit --allow-empty -m "[CircleCI Automation] $(git log develop -1 --pretty=%B)"
+
+git push -q https://${USER_NAME}:${GITHUB_TOKEN}@github.com/${USER_NAME}/rMaxiQp.github.io.git master
---
+++ index.md
@@ -0,0 +1,18 @@
+---
+# Feel free to add content and custom Front Matter to this file.
+# To modify the layout, see https://jekyllrb.com/docs/themes/#overriding-theme-defaults
+layout: default
+date:   2019-10-24
+categories: [jekyll]
+github: https://github.com/rMaxiQp
+---
+
+This is a website that I'm fooling around with.
+
+<ul>
+  {% for post in site.posts %}
+    <li>
+      <a href="{{ post.url }}">{{ post.title }}</a>
+    </li>
+  {% endfor %}
+</ul>
